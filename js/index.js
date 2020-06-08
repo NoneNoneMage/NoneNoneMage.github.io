18f00c05439b9b016cc4ea8ee8b3f378919c53c3
unknown <0027012421@iwhalecloud.com>
2020-06-08 16:18:57 +0800

    update

--- js/index.js
+++ js/index.js
@@ -9,10 +9,12 @@ function getQueryStringValue(name) {
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+let access_token = null;
+
 function getAccessToken(code,succ,error){
     var data = {
-        'client_id':'738993563306475',
-        'client_secret':'e843acefa8dd7a4cbfdf6ec243db1105',
+        'client_id':'3228493793906383',
+        'client_secret':'f04fcfe0966a9536d5f0ae6f2c7dd2bd',
         'redirect_uri':'https://nonenonemage.github.io/',
         'grant_type':'authorization_code',
         'code':code
@@ -32,6 +34,27 @@ function getAccessToken(code,succ,error){
     });
 }
 
+function getPersonalInfo() {
+    var params = {
+        'fields':'id,username',
+        'access_token':access_token
+    }
+    printEvent('getPersonalInfo GET', params);
+
+    $.ajax({
+        type: 'GET',
+        url: "https://graph.instagram.com/me",
+        params:params,
+        success: (result)=>{
+            printEvent('getPersonalInfo RESP:',result)
+        },
+        error:(err)=>{
+            printEvent('getPersonalInfo Error:',result)
+        },
+        da
+    });
+}
+
 function printEvent(handler, event) {
     $('#printContainer pre').append(handler + ': ' + JSON.stringify(event, null, 4) + '\n\n');
     $('#printContainer').scrollTop($('#printContainer')[0].scrollHeight);
@@ -41,6 +64,7 @@ $(function() {
     var code = getQueryStringValue("code");
     if(code != undefined && code != ""){
         getAccessToken(code,function (result) {
+            access_token = result.access_token;
             $('#btnAccess').hide();
             $('#apiList').removeClass('hide').show();
             printEvent('getAccessToken RESP', result);
